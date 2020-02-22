9109e98ee9622ca1c657ffced19a0ac7415e52e6
kuelye <kuelye@gmail.com>
2020-02-22 23:18:28 +0300

    B: base map project

---
+++ b/src/main/java/com/kuelye/vkcup20ii/b/ui/activity/MapActivity.kt
@@ -0,0 +1,13 @@
+package com.kuelye.vkcup20ii.b.ui.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.kuelye.vkcup20ii.b.R
+
+class MapActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_map)
+    }
+}
---
+++ b/src/main/java/com/kuelye/vkcup20ii/b/ui/fragment/MapFragment.kt
@@ -0,0 +1,5 @@
+package com.kuelye.vkcup20ii.b.ui.fragment
+
+import com.google.android.gms.maps.MapFragment
+
+class MapFragment : MapFragment()

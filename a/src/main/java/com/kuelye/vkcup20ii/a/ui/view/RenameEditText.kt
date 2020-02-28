7c3a6877cab3cc0e7363579dec9cf2d0e04b0412
kuelye <kuelye@gmail.com>
2020-02-28 13:05:07 +0300

    A: rename+

--- a/src/main/java/com/kuelye/vkcup20ii/a/ui/view/RenameEditText.kt
+++ a/src/main/java/com/kuelye/vkcup20ii/a/ui/view/RenameEditText.kt
@@ -7,22 +7,29 @@ import android.graphics.Paint
 import android.graphics.Paint.ANTI_ALIAS_FLAG
 import android.graphics.Paint.Style.FILL
 import android.util.AttributeSet
+import android.util.Log
 import android.view.animation.AccelerateDecelerateInterpolator
 import android.widget.EditText
 import androidx.appcompat.widget.AppCompatEditText
 import com.kuelye.vkcup20ii.a.R
 import com.kuelye.vkcup20ii.core.utils.color
+import com.kuelye.vkcup20ii.core.utils.dimen
 import com.kuelye.vkcup20ii.core.utils.modifyAlpha
 
 class RenameEditText @JvmOverloads constructor(
     context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
 ) : AppCompatEditText(context, attrs, defStyleAttr) {
 
+    companion object {
+        private val TAG = RenameEditText::class.java.simpleName
+    }
+
     private var backgroundAlpha: Float = 0f
     private var backgroundAnimator: ValueAnimator? = null
     private var animatorEnabledTarget: Boolean? = null
 
     private val backgroundColor = color(R.color.filled_box_edit_text_fill_color)
+    private val backgroundCornerRadius = dimen(R.dimen.filled_box_edit_text_corner_radius_small)
 
     private val paint = Paint().apply {
         style = FILL
@@ -30,10 +37,16 @@ class RenameEditText @JvmOverloads constructor(
         flags = ANTI_ALIAS_FLAG
     }
 
+    override fun onPreDraw(): Boolean {
+        return super.onPreDraw()
+    }
+
     override fun onDraw(canvas: Canvas) {
         if (backgroundAlpha > 0) {
             paint.color = backgroundColor.modifyAlpha(backgroundAlpha)
-            canvas.drawRoundRect(0f, 0f, width.toFloat(), height.toFloat(), 10f, 10f, paint)
+            val left = scrollX.toFloat()
+            canvas.drawRoundRect(left, 0f, left + width, height.toFloat(),
+                backgroundCornerRadius.toFloat(), backgroundCornerRadius.toFloat(), paint)
         }
         super.onDraw(canvas)
     }
